b71dd777577f24c9c4d71baffa19df3f4da3800c
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-24 02:27:38 +0800

    remove col categories from task and added user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_123549) do
+ActiveRecord::Schema.define(version: 2021_03_23_182510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 2021_03_23_123549) do
   create_table "tasks", force: :cascade do |t|
     t.string "item"
     t.text "description"
-    t.integer "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
