8e6ff92095365c3ca7789bc5a473c131a68f51d2
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-25 07:04:54 +0800

    set up auth using Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tasks
   resources :categories
   root 'pages#home'
   get '/home' => 'pages#home'
   get '/about' => 'pages#about'
-  get '/signup' => 'users#new'
    
 end
