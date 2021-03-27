fa43c393faa79a9feb2cc094d7a591f827747765
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-27 09:05:29 +0800

    added profile page and authentication for it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
+    before_action :authenticate_user!, only: [:profile]
+    #before anyone can go to the views of each method or route in this controller, they must first be logged in. I'm using it only on profile.
     def home
     end
 
     def about
+    end
+
+    def profile
     end
 end
   
