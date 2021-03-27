e58795a42a3d4da85b1cf7a3f3662de6a2a3a7d3
Elijah Arcedera <eli.arcedera@gmail.com>
2021-03-27 09:08:29 +0800

    clean up code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, :only => [:show]
   resources :tasks
   resources :categories
   root 'pages#home'
   get '/home' => 'pages#home'
   get '/about' => 'pages#about'
-   
+  get '/profile' => 'pages#profile'
 end
