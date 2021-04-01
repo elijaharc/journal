8ac740486ee2c35c097c9cab0865757d59cbe24e
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-01 09:16:18 +0800

    destroys categories and tasks when user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :categories
+  has_many :categories, :dependent => :destroy
   validates :username, presence: true
   validates :username, uniqueness: true, if: -> { self.username.present? } 
   # Include default devise modules. Others available are:
