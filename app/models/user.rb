77dd1892144c299945faadb39234cdb0f75a519c
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-02 18:10:18 +0800

    add has_many through: for user to access all tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :categories, :dependent => :destroy
+  has_many :tasks, through: :categories
   validates :username, presence: true
   validates :username, uniqueness: true, if: -> { self.username.present? } 
   # Include default devise modules. Others available are:
