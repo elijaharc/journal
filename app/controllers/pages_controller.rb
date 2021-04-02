f7125d806784b7783b0c5ca17bf47e46e98e2a72
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-02 18:06:20 +0800

    added show all tasks in pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    before_action :authenticate_user!, only: [:profile]
+    before_action :authenticate_user!, only: [:profile, :tasks]
     #before anyone can go to the views of each method or route in this controller, they must first be logged in. I'm using it only on profile.
     def home
     end
@@ -9,5 +9,10 @@ class PagesController < ApplicationController
 
     def profile
     end
+
+    def tasks
+        @tasks = current_user.tasks.order('deadline asc')
+    end
 end
-  
+
+# .paginate(page: params[:page], per_page: 3).order('created_at desc')
