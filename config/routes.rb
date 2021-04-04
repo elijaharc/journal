3c4661559f96fb1df80c8a89458f26f057d837fb
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-04 19:44:37 +0800

    for heroku pg error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => :registrations }
+  # devise_for :users, :controllers => { :registrations => :registrations }
   resources :users, :only => [:show, :update]
   # get '/categories/:id/tasks' => 'tasks#show'
   resources :categories do
