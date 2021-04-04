53cbbdfe9199405e33338b4303592343216eb792
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-04 19:27:58 +0800

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, only: [:show]
+    before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
   def index
@@ -18,21 +19,19 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /categories/1 or /categories/1.json
   def update
-    respond_to do |format|
-      if @current_user.update(user_params)
-        format.html { redirect_to current_user, notice: "You have successfully updated your profile." }
-        
-      else
-        format.html { render :edit }
-
-      end
-    end
+      # if @current_user.update(user_params)
+      #   # redirect_to categories_path
+      # else
+      #   render action: 'edit'
+      # end
   end
 
   private
-
+    def set_user
+      @user = current_user.id
+    end
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :first_name, :last_name, :email)
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :order_categories_by)
     end
 end
