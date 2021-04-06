d422b36ee980f079acdaea41520c65065f7aca5f
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-07 03:31:13 +0800

    required first&last name + freeze order options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   has_many :categories, :dependent => :destroy
   has_many :tasks, through: :categories
-  validates :username, presence: true
+  validates :username, :first_name, :last_name, presence: true
   validates :username, uniqueness: true, if: -> { self.username.present? } 
   validates_confirmation_of :password
   ORDER_CATEGORY_OPTIONS = [
     ['Alphabetical (Ascending)', 'category_name asc'], ['Alphabetical (Descending)', 'category_name desc'],
     ['First Created', 'created_at asc'], ['Last Created', 'created_at desc'], ['Last Edited', 'updated_at desc']
-]
+  ].freeze
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
